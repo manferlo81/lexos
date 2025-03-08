339343c2e26503551a9175f7bec8cd197dd81339
Manuel Fernández <manferlo81@gmail.com>
2025-03-08 13:39:42 +0700

    chore(test): Test exports

---
+++ __test__/exports.test.ts
@@ -0,0 +1,23 @@
+import * as lexos from '../src'
+
+type ExportName = keyof typeof lexos
+
+describe('exports', () => {
+
+  test('should export createLexer function', () => {
+    const expected: Record<ExportName, unknown> = {
+      createLexer: expect.any(Function),
+      regexpTest: expect.any(Function),
+      stringTest: expect.any(Function),
+      oneOfTest: expect.any(Function),
+      anyOfTest: expect.any(Function),
+      sequentialTest: expect.any(Function),
+      rule: expect.any(Function),
+      regexpRule: expect.any(Function),
+      stringRule: expect.any(Function),
+      parentRule: expect.any(Function),
+    }
+    expect(lexos).toEqual(expected)
+  })
+
+})
