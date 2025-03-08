185854d8deef68ab829bb8fa202a84bddb430c82
Manuel Fernández <manferlo81@gmail.com>
2025-03-08 17:35:57 +0700

    chore: Fix wrong import path

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,6 +1,7 @@
-import type { Tokenizer, TokenType } from '.'
 import { tokenizeCode } from './tokenize'
 import type { RuleList } from './types/rule-types'
+import type { TokenType } from './types/token-types'
+import type { Tokenizer } from './types/types'
 
 export function createLexer<T extends TokenType = never>(rules: RuleList<T>): Tokenizer<T> {
   return (code) => {
