ecee82135e14598c09b48d7d9ea299256c539897
Manuel Fernández <manferlo81@gmail.com>
2025-03-21 13:38:21 +0700

    chore(test): Add test fro internal `createGetNextToken` function

---
+++ __test__/internal/createGetNextToken.test.ts
@@ -0,0 +1,23 @@
+import type { TokenGenerator } from '../../src'
+import { initTokenGenerator } from '../../src'
+import { createGetNextToken } from '../../src/tools/get-next-token'
+
+describe('createGetNextToken internal function', () => {
+
+  test('should create getNextToken function', () => {
+    const createTokenGenerator = initTokenGenerator([])
+    const generator = createTokenGenerator('', 0)
+    const getNextToken = createGetNextToken(generator)
+    expect(typeof getNextToken === 'function').toBe(true)
+  })
+
+  test('should return tokens from generator', () => {
+    const createTokenGeneratorMockUp = function* (): TokenGenerator<'NUM', 'LT'> {
+      yield { type: 'NUM', value: '10', pos: 0 }
+      yield { type: 'LT', pos: 2 }
+    }
+    const getNextToken = createGetNextToken(createTokenGeneratorMockUp())
+    expect([getNextToken(), getNextToken(), getNextToken()]).toEqual([...createTokenGeneratorMockUp(), null])
+  })
+
+})
