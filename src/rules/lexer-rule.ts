ed0e147d236198adc6f03a029250f046e2c85c0e
Manuel Fernández <manferlo81@gmail.com>
2025-09-09 21:59:40 +0700

    chore: Fix some more types

--- src/rules/lexer-rule.ts
+++ src/rules/lexer-rule.ts
@@ -2,14 +2,16 @@ import { initTokenGenerator } from '../main/generator'
 import { ruleTest } from '../tests/rule-test'
 import { createRule } from '../tools/create-rule'
 import type { Falsy, PotentiallyFalsy } from '../types/helper-types'
-import type { MultiTokenRule } from '../types/rule-multi-types'
+import type { MultiTokenRule, MultiTokenRuleNoLastToken } from '../types/rule-multi-types'
 import type { Rule, RuleList, UnifiableRules } from '../types/rule-types'
 import type { AnyTest } from '../types/test-types'
 import type { TokenType } from '../types/token-types'
 
+export function lexerRule<T extends TokenType = never>(test: AnyTest, rule: Rule<T, never>, lastTokenType?: Falsy): MultiTokenRuleNoLastToken<T>
 export function lexerRule<T extends TokenType = never, L extends TokenType = never>(test: AnyTest, rule: Rule<T, L>, lastTokenType?: Falsy): MultiTokenRule<T, L>
 export function lexerRule<T extends TokenType = never, L extends TokenType = never>(test: AnyTest, rule: Rule<T, L>, lastTokenType: L): MultiTokenRule<T, L>
 
+export function lexerRule<T extends TokenType = never>(test: AnyTest, rules: RuleList<T, never>, lastTokenType?: Falsy): MultiTokenRuleNoLastToken<T>
 export function lexerRule<T extends TokenType = never, L extends TokenType = never>(test: AnyTest, rules: RuleList<T, L>, lastTokenType?: Falsy): MultiTokenRule<T, L>
 export function lexerRule<T extends TokenType = never, L extends TokenType = never>(test: AnyTest, rules: RuleList<T, L>, lastTokenType: L): MultiTokenRule<T, L>
 
--- src/types/rule-multi-types.ts
+++ src/types/rule-multi-types.ts
@@ -1,11 +1,16 @@
 import type { PotentiallyFalsy, RuleBase, WithLength } from './helper-types'
 import type { TokenType } from './token-types'
-import type { TokenGenerator } from './types'
+import type { TokenGenerator, TokenGeneratorNoLastToken } from './types'
 
 export interface MultiTokenRuleResult<T extends TokenType, L extends TokenType> extends WithLength {
   generator: TokenGenerator<T, L>
 }
 
+export interface MultiTokenRuleResultNoLastToken<T extends TokenType> extends WithLength {
+  generator: TokenGeneratorNoLastToken<T>
+}
+
 export type PotentialMultiTokenRuleResult<T extends TokenType, L extends TokenType> = PotentiallyFalsy<MultiTokenRuleResult<T, L>>
 
 export type MultiTokenRule<T extends TokenType, L extends TokenType> = RuleBase<MultiTokenRuleResult<T, L>>
+export type MultiTokenRuleNoLastToken<T extends TokenType> = RuleBase<MultiTokenRuleResultNoLastToken<T>>
