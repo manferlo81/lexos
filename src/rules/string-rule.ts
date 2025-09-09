59ec176098b042160c8b7581688ef9345817b8d9
Manuel Fernández <manferlo81@gmail.com>
2025-09-09 16:53:05 +0700

    chore: Make `stringRule` and `stringTest` array patterns readonly

--- src/rules/string-rule.ts
+++ src/rules/string-rule.ts
@@ -8,36 +8,36 @@ import type { GetTokenType, TokenType } from '../types/token-types'
 
 // Using get type returning falsy
 
-export function stringRule(getType: GetFalsyTokenType, value: number): ValueTest
-export function stringRule(getType: GetFalsyTokenType, values: number[]): ValueTest
+export function stringRule(getType: GetFalsyTokenType, number: number): ValueTest
+export function stringRule(getType: GetFalsyTokenType, numbers: readonly number[]): ValueTest
 export function stringRule(getType: GetFalsyTokenType, value: string, insensitive?: boolean): ValueTest
-export function stringRule(getType: GetFalsyTokenType, values: StringifyableTest[], insensitive?: boolean): ValueTest
-export function stringRule(getType: GetFalsyTokenType, stringifyable: StringifyableTest | StringifyableTest[], insensitive?: boolean): ValueTest
+export function stringRule(getType: GetFalsyTokenType, values: readonly StringifyableTest[], insensitive?: boolean): ValueTest
+export function stringRule(getType: GetFalsyTokenType, stringifyable: StringifyableTest | readonly StringifyableTest[], insensitive?: boolean): ValueTest
 
 // Using get type returning a type
 
-export function stringRule<T extends TokenType, V extends number>(getType: GetActualTokenType<T>, value: V): SingleTokenRule<T, `${V}`>
-export function stringRule<T extends TokenType, V extends number>(getType: GetActualTokenType<T>, values: V[]): SingleTokenRule<T, `${V}`>
+export function stringRule<T extends TokenType, V extends number>(getType: GetActualTokenType<T>, number: V): SingleTokenRule<T, `${V}`>
+export function stringRule<T extends TokenType, V extends number>(getType: GetActualTokenType<T>, numbers: readonly V[]): SingleTokenRule<T, `${V}`>
 export function stringRule<T extends TokenType>(getType: GetActualTokenType<T>, value: string, insensitive: true): SingleTokenRule<T>
 export function stringRule<T extends TokenType, V extends string>(getType: GetActualTokenType<T>, value: V, insensitive?: false): SingleTokenRule<T, V>
-export function stringRule<T extends TokenType>(getType: GetActualTokenType<T>, values: StringifyableTest[], insensitive: true): SingleTokenRule<T>
-export function stringRule<T extends TokenType, V extends StringifyableTest>(getType: GetActualTokenType<T>, values: V[], insensitive?: false): SingleTokenRule<T, `${V}`>
-export function stringRule<T extends TokenType>(getType: GetActualTokenType<T>, stringifyable: StringifyableTest | StringifyableTest[], insensitive?: boolean): SingleTokenRule<T>
+export function stringRule<T extends TokenType>(getType: GetActualTokenType<T>, stringifyables: readonly StringifyableTest[], insensitive: true): SingleTokenRule<T>
+export function stringRule<T extends TokenType, V extends StringifyableTest>(getType: GetActualTokenType<T>, stringifyables: readonly V[], insensitive?: false): SingleTokenRule<T, `${V}`>
+export function stringRule<T extends TokenType>(getType: GetActualTokenType<T>, stringifyable: StringifyableTest | readonly StringifyableTest[], insensitive?: boolean): SingleTokenRule<T>
 
 // Using hard coded type
 
-export function stringRule<T extends TokenType, V extends number>(type: T, value: V): SingleTokenRule<T, `${V}`>
-export function stringRule<T extends TokenType, V extends number>(type: T, values: V[]): SingleTokenRule<T, `${V}`>
+export function stringRule<T extends TokenType, V extends number>(type: T, number: V): SingleTokenRule<T, `${V}`>
+export function stringRule<T extends TokenType, V extends number>(type: T, numbers: readonly V[]): SingleTokenRule<T, `${V}`>
 export function stringRule<T extends TokenType>(type: T, value: string, insensitive: true): SingleTokenRule<T>
 export function stringRule<T extends TokenType, V extends string = string>(type: T, value: V, insensitive?: false): SingleTokenRule<T, V>
-export function stringRule<T extends TokenType>(type: T, values: StringifyableTest[], insensitive: true): SingleTokenRule<T>
-export function stringRule<T extends TokenType, V extends StringifyableTest>(type: T, values: V[], insensitive?: false): SingleTokenRule<T, `${V}`>
-export function stringRule<T extends TokenType>(type: T, stringifyable: StringifyableTest | StringifyableTest[], insensitive?: boolean): SingleTokenRule<T>
+export function stringRule<T extends TokenType>(type: T, stringifyables: readonly StringifyableTest[], insensitive: true): SingleTokenRule<T>
+export function stringRule<T extends TokenType, V extends StringifyableTest>(type: T, stringifyables: readonly V[], insensitive?: false): SingleTokenRule<T, `${V}`>
+export function stringRule<T extends TokenType>(type: T, stringifyable: StringifyableTest | readonly StringifyableTest[], insensitive?: boolean): SingleTokenRule<T>
 
 // Generic case
 
-export function stringRule<T extends TokenType>(type: T | GetTokenType<T>, stringifyable: StringifyableTest | StringifyableTest[], insensitive?: boolean): SingleTokenRuleOrTest<T>
-export function stringRule<T extends TokenType>(type: T | GetTokenType<T>, stringifyable: StringifyableTest | StringifyableTest[], insensitive?: boolean): SingleTokenRuleOrTest<T> {
+export function stringRule<T extends TokenType>(type: T | GetTokenType<T>, stringifyable: StringifyableTest | readonly StringifyableTest[], insensitive?: boolean): SingleTokenRuleOrTest<T>
+export function stringRule<T extends TokenType>(type: T | GetTokenType<T>, stringifyable: StringifyableTest | readonly StringifyableTest[], insensitive?: boolean): SingleTokenRuleOrTest<T> {
   return singleTokenRule(
     type,
     stringTest(stringifyable, insensitive),
--- src/tools/map-items.ts
+++ src/tools/map-items.ts
@@ -1,8 +1,9 @@
 type TransformItem<T, R, A extends unknown[] = []> = (item: T, ...args: A) => R
 
-export function mapItemsWithArgs<T, R>(items: T[], transformItem: TransformItem<T, R>): R[]
-export function mapItemsWithArgs<T, R, A extends unknown[]>(items: T[], transformItem: TransformItem<T, R, A>, ...args: A): R[]
-export function mapItemsWithArgs<T, R, A extends unknown[]>(items: T[], transformItem: TransformItem<T, R, A>, ...args: A): R[] {
-  const mapFunction = (item: T): R => transformItem(item, ...args)
-  return items.map(mapFunction)
+export function mapItemsWithArgs<T, R>(items: readonly T[], transformItem: TransformItem<T, R>): R[]
+export function mapItemsWithArgs<T, R, A extends unknown[]>(items: readonly T[], transformItem: TransformItem<T, R, A>, ...args: A): R[]
+export function mapItemsWithArgs<T, R, A extends unknown[]>(items: readonly T[], transformItem: TransformItem<T, R, A>, ...args: A): R[] {
+  return items.map(
+    (item: T): R => transformItem(item, ...args),
+  )
 }
